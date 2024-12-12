50ba5532adcb6d07de9ca9f83d5559465436d1f4
Naseem Akhtar <naseem.10748435@ltimindtree.com>
2024-12-12 12:41:52 +0530

    ui fixing

--- src/components/cardLoader.js
+++ src/components/cardLoader.js
@@ -5,15 +5,26 @@ function CardLoader() {
    
     return (
         <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
+            <Placeholder  />
             <Card.Body>
                 <Placeholder as={Card.Title} animation="glow">
                     <Placeholder xs={6} />
                     <Placeholder xs={6} />
+                    <Placeholder xs={6} />
+                    <Placeholder xs={6} />
                 </Placeholder>
                 <Placeholder as={Card.Text} animation="glow">
-                    <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                    <Placeholder xs={6} /> <Placeholder xs={8} />
+                    <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={12} /> {' '}
+                    <Placeholder xs={6} /> <Placeholder xs={5} /> <Placeholder xs={12} />
                 </Placeholder>
             </Card.Body>
         </Card>
